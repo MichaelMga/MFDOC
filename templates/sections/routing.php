c9972f7c88545042e96d68b40dfc732a11a1d0af
michael manga <middleweightsoul@gmail.com>
2021-06-16 23:50:11 +0200

    updating the routing page

--- templates/sections/routing.php
+++ templates/sections/routing.php
@@ -23,57 +23,48 @@ Like stated in the <a href="#">get started </a> section, using the htaccess file
 
 
 
-  You need to register your routes in the routeArray, that you can find in the "Routes.php" file (for now, the array is totally empty).   </br>
+  You need to register your routes in the routeArray, that you can find in the "Routes.php" file (for now, the array is totally empty). </br>
 
 
    <img src="public/assets/img/routing2.PNG"> </br>
 
 
 
-  Let's create our first route in it.
+   <h5> Let's create our first route in it. </h5> </br>
 
-  Here is a prototype root : [exampleUrl =>  [ "name" => "exampleName" , "controller" => "exampleController" , "method" => "exampleMethod" , "parameters" => ["exampleParam1", "exampleParam1"] ];
-
-
-  Using this, let's create our welcome route.
+  Here is a prototype root : [exampleUrl =>  [ "name" => "exampleName" , "controller" => "exampleController" , "method" => "exampleMethod" , "parameters" => ["exampleParam1", "exampleParam1"] ]; </br></br>
 
 
-  $map =  [rootUrl =>  [ "name" => "exampleName" , "controller" => "exampleController" , "method" => "exampleMethod" , "parameters" => ["exampleParam1", "exampleParam1"] ]
+  Using this, let's create our welcome route.</br>
+
+
+  $routes =  [rootUrl =>  [ "name" => "exampleName" , "controller" => "exampleController" , "method" => "exampleMethod" , "parameters" => ["exampleParam1", "exampleParam1"] ]; </br></br>
 
   
-  What is the rootUrl constant? 
+  What is the rootUrl constant? .</br></br>
+ 
+ As stated here, you can access, in your whole application, all constants define in the file : const.php .</br></br>
 
- As stated here, you can access, in your whole application, all constants define in the file : const.php
-
-      One of the constant is : rootUrl. By default, it is set at the root of your folder.
-
-
-  Now, lets go element by element.
-
-  Obviously, you can imagine the "name" attribute represents the name you want to give to your route.
+      One of the constant is : rootUrl. By default, it is set at the root of your folder.</br></br></br>
 
 
-  Now, the controller attribute. Simply enter the name of the controller you already created in the folder "controllers". The Framework will authomatically call it when the request is done.
+      <h5> Now, lets go element by element </h5></br></br>
 
-  You can imagine the method attribute refers to a method of this controller.
+  Obviously, you can imagine the <b>"name"</b> attribute represents the name you want to give to your route. </br> </br>
 
-  Finally, the parameters attribute refers to the parameters to pass to the method we just mentioned (it can be empty).
+
+  -Now, the <b>"controller" </b> attribute.</br> </br> 
+  
+  
+  Simply enter the name of the controller you already created in the folder <b>"controllers"</b>.</br></br> 
+  
+  The Framework will authomatically call it when the request is done. .</br></br> 
+
+  You can imagine the  <b>"method"</b> attribute refers to a method of this controller. .</br></br> 
+
+  Finally, the <b>"parameters"</b> attribute refers to the parameters to pass to the method we just mentioned (it can be empty). </br> </br>
   
 
-
-
-
-      //const.php
-
-
-      define("rootUrl", "const.php");
-
-   
-   Let's imagine your application is hosted on : 
-
-   www.myApp.fr
-
-
-
+  </br></br></br>
 
 
